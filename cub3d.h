5e313314a991e34bbd88e919d9fd5483cb0df0d6
Paul-Hartman <paul.hartman222@gmail.com>
2024-09-27 18:15:02 +0200

    working on structs

---
+++ cub3d.h
@@ -0,0 +1,42 @@
+# ifndef CUBE3D_H
+#define CUBE3D_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include "ft_printf/ft_printf.h"
+# include "ft_printf/libft/libft.h"
+# include "mlx/mlx.h"
+# include <X11/Xlib.h>
+# include <X11/keysym.h>
+
+#define FOV 60
+#define CAR_HEIGHT 32
+#define WALL_HEIGHT 64
+
+typedef struct s_ray
+{
+	float x;
+	float y;
+	float dx;
+	float dy;
+
+}				t_ray;
+
+typedef struct s_player
+{
+	float x;
+	float y;
+	float angle;
+}				t_player;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	int		screenwidth;
+	int		screenheight;
+}				t_vars;
+
+#endif
